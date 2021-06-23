d194b0913294546a76601c293cb53b793ae47b63
goncalo-mendes <goncalo.mendes.dev@gmail.com>
2021-06-23 09:51:20 +0100

    iniciar proj e firebase

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/button";
+
+
+function App() {
+  return (
+    <div>
+      <Button text= "texto2"/>
+      <Button />    
+    </div>
+    
+  );
+}
+
+export default App;
